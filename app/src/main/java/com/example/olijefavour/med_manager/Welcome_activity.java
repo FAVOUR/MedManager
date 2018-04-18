7e5321b1d2632ccfd37df9945e59e7bdc8c321bf
Olije Favour <TRUELYFAVOURED@GMAIL.COM>
2018-04-18 23:08:54 +0100

    Update Welcome_activity.java

--- app/src/main/java/com/example/olijefavour/med_manager/Welcome_activity.java
+++ app/src/main/java/com/example/olijefavour/med_manager/Welcome_activity.java
@@ -8,6 +8,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
 public class Welcome_activity extends AppCompatActivity {
 
     @Override
